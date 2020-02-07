efe8f2863bff827547fe08f4b0f5d25c31131fbb
cschafer <cschafer@dataminr.com>
2020-02-07 09:05:17 -0500

    Disable rate limiting for now

--- reverse-proxy/src/github.com/cts5ws/ratelim/ratelim.go
+++ reverse-proxy/src/github.com/cts5ws/ratelim/ratelim.go
@@ -5,12 +5,17 @@ import (
 	"time"
 )
 
+var ratelimEnable = false;
 var requestWaitTime, _ = time.ParseDuration("10s")
 
 var ipAddrToLastRequest = make(map[string]time.Time)
 
 // todo: lock map access. might not be thread safe
 func ShouldRateLimit(ip net.IP) bool {
+
+	if !ratelimEnable {
+		return false;
+	}
 
 	currentTime := time.Now()
 
